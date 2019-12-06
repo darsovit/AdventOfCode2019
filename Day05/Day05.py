062525ecbe640ad7c91c5db3a6efd32fdb8992ed
Jonathan Cameron <darsovit@gmail.com>
2019-12-05 20:35:53 -0700

    Advent of Code 2019, Day 5, Part 1 Solved
    Completion of the first part of the program, solved.
    
    Silly me, calling two functions readInput(). Ooops.
    
    http://adventofcode.com/2019/day/5

--- Day05/Day05.py
+++ Day05/Day05.py
@@ -1,6 +1,7 @@
 #! python
 
 def readInput():
+    print("Read Input: 1")
     return 1
 
 def sendOutput(val):
@@ -10,10 +11,12 @@ def halt():
     print( "HALT" )
 
 def store( code, z, val ):
-    print( code, z, val )
+    #print( 'store before:', code[z], val )
     code[z] = val
+    #print( 'store after:', code[z], val )
 
-def getInstr( code, instruction ):
+def getInstr( code, ip ):
+    instruction = code[ip] % 100
     if instruction == 1:
         return (lambda x,y,z: store(code, z, x + y), 4, 2)
     elif instruction == 2:
@@ -24,9 +27,11 @@ def getInstr( code, instruction ):
         return (lambda z: sendOutput( z ), 2, 1)
     elif instruction == 99:
         return (halt, 1)
+    else:
+        print('Invalid instruction: ', instruction )
 
 def buildCommand( code, ip ):
-    (instruction,size,numInput) = getInstr( code, code[ip] % 100 )
+    (instruction,size,numInput) = getInstr( code, ip )
     immediateLambda = lambda x: x
     addressingLambda = lambda x: code[x]
     
@@ -50,6 +55,7 @@ def Intcode(code):
     opmul  = 2
     while ( code[ip] != ophalt ):
         (instruction,size) = buildCommand(code, ip)
+        #print( ' '.join( map(str, code[ip:ip+size] ) ) )
         if size == 4:
             instruction( code[ip+1], code[ip+2], code[ip+3] )
         elif size == 2:
@@ -58,18 +64,29 @@ def Intcode(code):
             instruction()
         ip += size
 
-    print( "Intcode result:", code )
+    print( "Intcode result:", code[0] )
     return code
+
+def PrintProgram( code ):
+    print( code )
+    ip = 0
+    ophalt = 99
+    while ( code[ip] != ophalt ):
+        (instruction, size) = buildCommand( code, ip )
+        print( ' '.join( map(str, code[ip:ip+size] ) ) )
+        ip = ip + size
+
 
 def test():
     assert( [99] == Intcode([99]) )
     assert( [2,0,0,0,99] == Intcode([1,0,0,0,99]) )
+    assert( [1, 7, 6, 6, 99, 0, 2, 1] == Intcode([1, 7, 6, 6, 99, 0, 1, 1]) )
     assert( [48,23,25,0,99] == Intcode([1101,23,25,0,99]) )
     assert( [2,3,0,6,99] == Intcode([2,3,0,3,99]) )
     assert( [2,4,4,5,99,9801] == Intcode([2,4,4,5,99,0]) )
     assert( [30,1,1,4,2,5,6,0,99] == Intcode([1,1,1,4,99,5,6,0,99]) )
 
-def readInput():
+def readProgInput():
     with open('input.txt') as f:
         line = str.rstrip( f.readline() )
         return list( map(int, line.split(',') ) )
@@ -87,7 +104,7 @@ def find_noun_verb_for_output(prog, expected):
                 return (noun,verb)
 
 test()
-
-prog = readInput()
+print("Test complete")
+prog = readProgInput()
 
 Intcode( prog )
